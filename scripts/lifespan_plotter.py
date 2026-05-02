22a23c52e7af7d1b0fc799fb7ee7d706ace82934
Antonius Torode <awtorode@gmail.com>
2026-05-02 13:47:46 -0500

    Fixed and updated some index formatting. Corrected error in lifespan plot, and added some to genesis

--- scripts/lifespan_plotter.py
+++ scripts/lifespan_plotter.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 
 # Example data: (Name, Birth Year, Lifespan)
 people = [
-    ("Adam", 0, 930),
-    ("Seth", 130, 912),
-    ("Enosh", 235, 905),
-    ("Cainan", 325, 910),
-    ("Mahalalel", 395, 895),
-    ("Jared", 460, 962),
-    ("Enoch", 622, 365),
-    ("Methuselah", 687, 969),
-    ("Lamech", 874, 777),
-    ("Noah", 1056, 950),
-    ("Shem", 1558, 500),
-    ("Arphaxad", 1658, 403),
-    ("Salah", 1693, 403),
-    ("Eber", 1723, 430),
-    ("Peleg", 1757, 209),
-    ("Reu", 1787, 207),
-    ("Serug", 1819, 200),
-    ("Nahor", 1849, 119),
-    ("Terah", 1878, 205),
-    ("Abram", 1948, 170)
+    ("Adam", 0, 930),        # Genesis 5
+    ("Seth", 130, 912),      # Genesis 5
+    ("Enosh", 235, 905),     # Genesis 5
+    ("Cainan", 325, 910),    # Genesis 5
+    ("Mahalalel", 395, 895), # Genesis 5
+    ("Jared", 460, 962),     # Genesis 5
+    ("Enoch", 622, 365),     # Genesis
+    ("Methuselah", 687, 969),# Genesis 5
+    ("Lamech", 874, 777),    # Genesis 5
+    ("Noah", 1056, 950),     # Genesis 9
+    ("Shem", 1558, 500),     # Genesis 11
+    ("Arphaxad", 1658, 403), # Genesis 11
+    ("Salah", 1693, 403),    # Genesis 11
+    ("Eber", 1723, 430),     # Genesis 11
+    ("Peleg", 1757, 209),    # Genesis 11
+    ("Reu", 1787, 207),      # Genesis 11
+    ("Serug", 1819, 200),    # Genesis 11
+    ("Nahor", 1849, 119),    # Genesis 11
+    ("Terah", 1878, 205),    # Genesis 11
+    ("Abram", 1948, 175)     # Genesiss 25:8
 ]
 
 # Sort people by birth year descending
